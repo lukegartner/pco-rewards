379274d0a95455e9076ecb0ee7803c7de61eb89a
Luke Gartner <lukegartner4@gmail.com>
2023-04-17 13:53:59 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Domain dev-h3q8th75zwvdmzlb.us.auth0.com
+// Client Id jcHj3b6PRxbzy2GmPq0sO2Kj8cgt5M9f
+// Slient Secret aH13tq0KwTV7a-1lyKrsWVyq09cQXTKi6qSH4g9RnsVPScjuxotY6H8SPcQo5BKg
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-h3q8th75zwvdmzlb.us.auth0.com"
+      clientId="jcHj3b6PRxbzy2GmPq0sO2Kj8cgt5M9f"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
